90ce5ed183cbf624e2e747b010898f57cdb24e6d
liyu <liyu@shopex.cn>
2022-02-15 18:38:50 +0800

    merge_sort

---
+++ design_linked_list.go
@@ -0,0 +1,89 @@
+package leetcode
+
+type MyLinkedList struct {
+	Val  int
+	Next *MyLinkedList
+}
+
+func Constructor() MyLinkedList {
+	return MyLinkedList{}
+}
+
+func (this *MyLinkedList) Get(index int) int {
+	for cur, count := this.Next, 0; cur != nil; cur, count = cur.Next, count+1 {
+		if count == index {
+			return cur.Val
+		}
+	}
+
+	return -1
+}
+
+func (this *MyLinkedList) AddAtHead(val int) {
+	if this.Next == nil {
+		this.Next = &MyLinkedList{
+			Val: val,
+		}
+		return
+	}
+
+	temp := this.Next
+	this.Next = &MyLinkedList{
+		Val:  val,
+		Next: temp,
+	}
+}
+
+func (this *MyLinkedList) AddAtTail(val int) {
+	if this.Next == nil {
+		this.Next = &MyLinkedList{
+			Val: val,
+		}
+		return
+	}
+	for cur := this.Next; cur != nil; cur = cur.Next {
+		if cur.Next == nil {
+			cur.Next = &MyLinkedList{
+				Val: val,
+			}
+			return
+		}
+	}
+}
+
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	if index <= 0 {
+		this.AddAtHead(val)
+		return
+	}
+
+	count := 0
+	pre := this
+	for cur := this.Next; cur != nil; pre, cur, count = cur, cur.Next, count+1 {
+		if count == index {
+			pre.Next = &MyLinkedList{
+				Val:  val,
+				Next: cur,
+			}
+			return
+		}
+	}
+
+	if count == index {
+		pre.Next = &MyLinkedList{
+			Val: val,
+		}
+	}
+}
+
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+
+	pre := this
+	for cur, count := this.Next, 0; cur != nil; pre, cur, count = cur, cur.Next, count+1 {
+
+		if count == index {
+			pre.Next = cur.Next
+			break
+		}
+	}
+}
---
+++ merge_sort.go
@@ -0,0 +1,38 @@
+package leetcode
+
+func mergeSort(nums []int) []int {
+
+	length := len(nums)
+	if length == 1 {
+		return nums
+	}
+	middle := length / 2
+
+	return merge(mergeSort(nums[0:middle]), mergeSort(nums[middle:length]))
+}
+
+func merge(leftNums []int, rightNums []int) []int {
+
+	leftNumsLength := len(leftNums)
+	rightNumsLength := len(rightNums)
+
+	var temp []int
+	l, r := 0, 0
+	for ; l < leftNumsLength && r < rightNumsLength; {
+		if leftNums[l] <= rightNums[r] {
+			temp = append(temp, leftNums[l])
+			l++
+		} else {
+			temp = append(temp, rightNums[r])
+			r++
+		}
+	}
+
+	if l == leftNumsLength {
+		temp = append(temp, rightNums[r: rightNumsLength]...)
+	} else {
+		temp = append(temp, leftNums[l: leftNumsLength]...)
+	}
+
+	return temp
+}
