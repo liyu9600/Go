b42173765eb393c3f90251ba5db9976447df0f85
liyu <liyu@shopex.cn>
2022-02-25 11:35:48 +0800

    climbing_stairs

---
+++ climbing_stairs.go
@@ -0,0 +1,33 @@
+package leetcode
+
+func climbStairs(n int) int {
+
+	/*if n == 1 {
+		return 1
+	}
+
+	if n == 2 {
+		return 2
+	}
+
+	res := climbStairs(n-1) + climbStairs(n - 2)
+
+	return res*/
+
+	if n == 1 {
+		return 1
+	}
+
+	if n == 2 {
+		return 2
+	}
+
+	res := 2
+	n1, n2 := 1, 2
+	for i := 3; i <= n; i++ {
+		res = n1 + n2
+		n1, n2 = n2, res
+	}
+
+	return res
+}
