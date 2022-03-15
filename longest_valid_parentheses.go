41fdb1cdd2ce6c2d1c51049dc32db177900bd265
liyu <liyu@shopex.cn>
2022-03-15 14:44:10 +0800

    longest_valid_parentheses

---
+++ longest_valid_parentheses.go
@@ -0,0 +1,29 @@
+package leetcode
+
+func longestValidParentheses(s string) int {
+
+	maxLength := 0
+	stack := []int{-1}
+
+	for i, v := range s {
+
+		if v == '(' {
+			stack = append(stack, i)
+			continue
+		}
+
+		l := len(stack)
+		if l > 1 {
+			stack = stack[0:l-1]
+			if r := i - stack[l-2]; r > maxLength {
+				maxLength = r
+			}
+
+			continue
+		}
+
+		stack[0] = i
+	}
+
+	return maxLength
+}
